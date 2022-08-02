819d9c237176e76449d6315fe6a435e5a86985ab
Leonardo Machado Fabris <leonardomachado200112@gmail.com>
2022-08-01 22:24:41 -0300

    scroll

--- src/components/PopularItens.tsx
+++ src/components/PopularItens.tsx
@@ -5,7 +5,7 @@ export const PopularItens = ({ list }: any) => {
         <div className="h-[360px] bg-[#222222] mt-10 w-[100%]">
             <div className="font-bold pl-4 pt-2 pb-2 border-b-[1px] border-zinc-600 ">Popular Today</div>
             <div className="block ">
-                <div className="flex">
+                <div className="flex overflow-x-scroll scrollHidden">
                     {list.map((i: any) => (
                         <ItemPopular key={i.id} item={i} />
                     ))}
